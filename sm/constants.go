9378109cb714a81480011002267be5363bac639c
ilyxenc <ilya.goldobin1@gmail.com>
2023-10-12 10:36:42 +0300

    added comments co constants

--- sm/constants.go
+++ sm/constants.go
@@ -1,7 +1,12 @@
 package sm
 
 const (
-	Xmlns        = "http://www.sitemaps.org/schemas/sitemap/0.9"
-	XmlnsXsi     = "http://www.w3.org/2001/XMLSchema-instance"
+	// Xmlns определяет пространство имен для стандарта карты сайта.
+	Xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9"
+
+	// XmlnsXsi определяет пространство имен для XML Schema Instance.
+	XmlnsXsi = "http://www.w3.org/2001/XMLSchema-instance"
+
+	// XsiSchemaLoc указывает на местоположение XML схемы для стандарта карты сайта.
 	XsiSchemaLoc = "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
 )
