4499bf7d7e63d1194f9c3a71c645ec596179b039
ilyxenc <ilya.goldobin1@gmail.com>
2023-10-12 10:36:21 +0300

    added comments to models

--- sm/models.go
+++ sm/models.go
@@ -5,18 +5,20 @@ import (
 	"time"
 )
 
+// Url представляет собой структуру данных для хранения информации о конкретном URL в карте сайта.
 type Url struct {
-	Loc        string    `xml:"loc"`
-	LastMod    time.Time `xml:"lastmod"`
-	ChangeFreq string    `xml:"changefreq,omitempty"`
-	Priority   float32   `xml:"priority,omitempty"`
+	Loc        string    `xml:"loc"`                  // Loc содержит адрес URL.
+	LastMod    time.Time `xml:"lastmod"`              // LastMod указывает на последнюю дату модификации URL.
+	ChangeFreq string    `xml:"changefreq,omitempty"` // ChangeFreq определяет частоту изменения содержимого URL.
+	Priority   float32   `xml:"priority,omitempty"`   // Priority устанавливает приоритет URL относительно других URL на вашем сайте.
 }
 
+// UrlSet представляет собой структуру данных для хранения множества URL и используется при сериализации и десериализации XML карты сайта.
 type UrlSet struct {
-	XMLName      xml.Name `xml:"urlset"`
-	Xmlns        string   `xml:"xmlns,attr"`
-	XmlnsXsi     string   `xml:"xmlns:xsi,attr"`
-	XsiSchemaLoc string   `xml:"xsi:schemaLocation,attr"`
+	XMLName      xml.Name `xml:"urlset"`                  // XMLName определяет корневой элемент XML.
+	Xmlns        string   `xml:"xmlns,attr"`              // Xmlns определяет пространство имен XML для карты сайта.
+	XmlnsXsi     string   `xml:"xmlns:xsi,attr"`          // XmlnsXsi определяет пространство имен XML Schema instance.
+	XsiSchemaLoc string   `xml:"xsi:schemaLocation,attr"` // XsiSchemaLoc указывает на расположение схемы XML.
 
-	Urls []Url `xml:"url"`
+	Urls []Url `xml:"url"` // Urls содержит срез URL, которые следует включить в карту сайта.
 }
